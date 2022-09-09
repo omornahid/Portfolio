038cba94c8e8ccf6cec623926433a9e9d703a55f
omornahid <omornahid@gmail.com>
2022-09-10 02:25:27 +0600

    fixed some typo

--- src/stores/cards.js
+++ src/stores/cards.js
@@ -18,7 +18,7 @@ export const cardStore = defineStore({
         name: "Weather App",
         preview: "/assets/screenshots/weather.png",
         description:
-          "It's a weather app.If someone search by a valid city name it will show the current temperature of that city by fetching api from OpenWeather.",
+          "It's a weather app.If someone search by a valid city name it will show the current temperature of that city by fetching API from OpenWeather.",
         prefix: "",
         appLink: "https://omornahid-weather-app.netlify.app/",
         repo: "https://github.com/omornahid/Weather-App",
@@ -28,7 +28,7 @@ export const cardStore = defineStore({
         name: "MoviHub",
         preview: "/assets/screenshots/moviehub.png",
         description:
-          "It's a movie app where people can see the trending movie list and can search for a movie. Here I used React Hooks, Bootstrap and fetched data from an API ",
+          "It's a movie app where people can see the trending movie list and can search for a movie. Here I used React Hooks, Bootstrap and fetched API from TMDB",
         appLink: "https://omornahid-movihub.netlify.app/",
         prefix: "https://",
         repo: "https://github.com/omornahid/MoviHub",
